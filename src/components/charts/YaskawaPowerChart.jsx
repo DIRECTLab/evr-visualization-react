4d3e4b97b53934363ddd405e63e5768ca4886aad
Jeff Anderson <foggeydoughtnut@gmail.com>
2023-02-15 14:29:23 -0700

    yaskawa chart is on the carousel

---
+++ src/components/charts/YaskawaPowerChart.jsx
@@ -0,0 +1,115 @@
+import { useEffect, useRef, useState } from "react";
+import moment from "moment";
+import {
+  Chart,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+  LineController,
+} from "chart.js"
+import { Line } from "react-chartjs-2";
+import api from "../../api";
+import Loading from "../Loading";
+
+Chart.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  LineController,
+  Title,
+  Tooltip,
+  Legend,
+  Filler
+)
+
+export const options = {
+  animation: {
+    duration: 0
+  },
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+  },
+  scales: {
+    y: {
+      min: 0,
+      max: 50,
+    },
+  },
+  maintainAspectRatio: true,
+}
+
+
+const YaskawaPowerChart = () => {
+  const [chartData, setChartData] = useState({});
+  const [loading, setLoading] = useState(true)
+  const chartRef = useRef(null);
+
+
+  
+  const loadData = async () => {
+    const res = await api.ems.yaskawa.get100()
+    if (res.error){
+      setLoading(false)
+    }
+    
+    const labels = res.data.map(data => moment(data.updatedAt).format('LTS'))
+    const data = res.data.map(data => data.activeAcPower)
+
+
+    setChartData({
+      labels: labels,
+      datasets: [
+        {
+          label: "Active AC Power",
+          data: data,
+          borderColor: 'rgba(75,192,192,1)',
+          backgroundColor: 'rgba(75,192,192,0.1)'
+        },
+      ]
+    })
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    setLoading(true)
+    loadData()
+    
+    const intervalId = setInterval(() => {
+      loadData()
+    }, 7000)
+
+    return () => {
+      clearInterval(intervalId); 
+    }
+  }, [])
+
+  if (loading) {
+    return (
+      <Loading />
+    )
+  }
+  else {
+    return(
+      <div className="w-full h-full">
+        <Line
+          datasetIdKey='id'
+          data={chartData}
+          options={options}
+          ref={chartRef}
+          redraw={false}
+          height={"300px"}
+        />
+      </div>
+    )
+  }
+}
+
+export default YaskawaPowerChart
--- src/components/home/GraphCarousel.jsx
+++ src/components/home/GraphCarousel.jsx
@@ -1,9 +1,86 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import YaskawaPowerChart from "../charts/YaskawaPowerChart";
+import api from "../../api";
 
 const GraphCarousel = () => {
+
+  const [yaskawaStatus, setYaskawaStatus] = useState(true)
+  const [loading, setLoading] = useState(true)
+
+  const checkStatus = async () => {
+    const yaskawaRes = await api.ems.yaskawa.getStatus()
+    if (yaskawaRes.error) {
+      return alert(yaskawaRes.error)
+    }
+    setYaskawaStatus(yaskawaRes.data.active)
+    setLoading(false)
+  }
+
+
+  useEffect(() => {
+    checkStatus()
+    const intervalId = setInterval(() => {
+      checkStatus()
+    }, 7000)
+    return () => {
+      clearInterval(intervalId); 
+    }
+  }, [])
+
   return (
-    <div>
-      <h1>THIS WILL BE THE GRAPH CAROUSEL</h1>
+    <div className="carousel w-full">
+      <div id="slide1" className="carousel-item relative w-full">
+      {/* ${'border-success' ? yaskawaStatus && !loading : 'border-error'} */}
+        <div className={`card w-1/3 shadow-xl mx-4 my-8 border-8 border-opacity-40 card-compact pt-1 ${(yaskawaStatus) ? 'border-success' : 'border-error'}`}>
+          <h2 className="card-title mt-0 justify-center">Yaskawa</h2>
+          <div className="card-body">
+            <YaskawaPowerChart />
+          </div>
+        </div>
+        {/* <div className="card w-1/3 shadow-xl mx-4 my-8 border-8 border-opacity-40 card-compact pt-1" classList={{'border-success': sma50Status() && allLoaded(), 'border-error': !sma50Status() && allLoaded()}}>
+          <h2 className="card-title mt-0 justify-center">SMA50</h2>
+          <div className="card-body">
+            <SMA50ACPowerChart />
+          </div>
+        </div>
+        <div className="card w-1/3 shadow-xl mx-4 my-8 border-8 border-opacity-40 card-compact pt-1" classList={{'border-success':  sma7Status() && allLoaded(), 'border-error': !sma7Status() && allLoaded()}}>
+          <h2 className="card-title mt-0 justify-center">SMA7</h2>
+          <div className="card-body">
+            <SMA7ACPowerChart />
+          </div>
+        </div>
+
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide2" className="btn btn-circle bg-opacity-50 border-none">❮</a> 
+          <a href="#slide2" className="btn btn-circle bg-opacity-50 border-none">❯</a>
+        </div> 
+      </div> 
+      <div id="slide2" className="carousel-item relative w-full">
+
+        <div className="card w-1/3 shadow-xl mx-4 my-8 border-8 border-opacity-40 card-compact pt-1" classList={{'border-success': symoStatus() && allLoaded(), 'border-error': !symoStatus() && allLoaded()}}>
+          <h2 className="card-title mt-0 justify-center">Symo</h2>
+          <div className="card-body">
+            <SymoAcPowerChart />
+          </div>
+        </div>
+        <div className="card w-1/3 shadow-xl mx-4 my-8 border-8 border-opacity-40 card-compact pt-1" classList={{'border-success': primoStatus() && allLoaded(), 'border-error': !primoStatus() && allLoaded()}}>
+          <h2 className="card-title mt-0 justify-center">Primo</h2>
+          <div className="card-body">
+            <PrimoAcPowerChart />
+          </div>
+        </div>
+
+        <div className="card w-1/3 shadow-xl mx-4 my-8 border-8 border-opacity-40 card-compact pt-1" classList={{'border-success': gustavStatus() && allLoaded(), 'border-error': !gustavStatus() && allLoaded()}}>
+          <h2 className="card-title mt-0 justify-center">Gustav</h2>
+          <div className="card-body">
+            <GustavVoltageChart />
+          </div>
+        </div>
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide1" className="btn btn-circle bg-opacity-50 border-none">❮</a> 
+          <a href="#slide1" className="btn btn-circle bg-opacity-50 border-none">❯</a>
+        </div>*/}
+      </div>  
     </div>
   )
 }
