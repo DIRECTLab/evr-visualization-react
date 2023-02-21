4d6d79a16a14b106848750d37e9c4fdfba0184c0
Jeff Anderson <foggeydoughtnut@gmail.com>
2023-02-21 11:49:24 -0700

    specific bus page done

--- src/components/buses/BusTable.jsx
+++ src/components/buses/BusTable.jsx
@@ -177,6 +177,7 @@ const BusTable = () => {
         </div>
       </div>
       
+      
     )
   }
 }
--- src/components/buses/SpecificBus.jsx
+++ src/components/buses/SpecificBus.jsx
@@ -16,6 +16,7 @@ import {
 import { Line } from "react-chartjs-2";
 import api from "../../api";
 import Loading from "../Loading";
+import { getCoreRowModel, getSortedRowModel, useReactTable, flexRender } from "@tanstack/react-table";
 
 Chart.register(
   CategoryScale,
@@ -105,6 +106,88 @@ const SpecificBus = ({id}) => {
     
   }
 
+
+  // TABLE SETUP
+  const columns = [
+    {
+      accessorKey: 'speed',
+      cell: info => Math.round(info.getValue() * 100) / 100,
+      header: () => <span>Speed</span>,
+    },
+    {
+      accessorKey: 'dcEnergyConsumptionKwh',
+      cell: info => Math.round(info.getValue() * 100) / 100,
+      header: () => <span>DC Consumption Kwh</span>,
+    },
+    {
+      accessorKey: 'directionReference',
+      cell: info => info.getValue(),
+      header: () => <span>Direction</span>,
+    },
+    {
+      accessorKey: 'latitude',
+      cell: info => info.getValue(),
+      header: () => <span>Latitude</span>,
+    },
+    {
+      accessorKey: 'longitude',
+      cell: info => info.getValue(),
+      header: () => <span>Longitude</span>,
+    },
+    {
+      accessorKey: 'soc',
+      cell: info => Math.round(info.getValue() * 100) / 100,
+      header: () => <span>SOC</span>,
+    },
+    {
+      accessorKey: 'lineName',
+      cell: info => info.getValue(),
+      header: () => <span>Line Name</span>,
+    },
+    {
+      accessorKey: 'timeToEmpty',
+      cell: info => Math.round(info.getValue() * 100) / 100,
+      header: () => <span>Time to Empty</span>,
+    },
+    {
+      accessorKey: 'totalVehicleDistance',
+      cell: info => Math.round(info.getValue() * 100) / 100,
+      header: () => <span>Total Distance</span>,
+    },
+    {
+      accessorFn: row => moment(row.createdAt).format('lll'),
+      cell: info => info.getValue(),
+      header: () => <span>GPS Fix Time</span>,
+      id: 'gpsFixTime',
+    },
+  ]
+
+  const updateFilter = () => {
+    if (searchFilter === ''){
+      return setBuses(allBuses)
+    }
+
+    const filtered = allBuses().filter(value => {
+      for (let key of Object.keys(value)){
+        if (key === 'gpsFixTime' && moment(`${value[key]}`).format('lll').includes(searchFilter)){
+          return true
+        }
+        if (`${value[key]}`.toLowerCase().includes(searchFilter.toLowerCase())){
+          return true
+        }
+      }
+      return false
+    })
+    setBuses(filtered)
+  }
+
+  const table = useReactTable({
+    data: buses,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+  })
+
   useEffect(() => {
     setLoading(true)
     loadData()
@@ -126,15 +209,56 @@ const SpecificBus = ({id}) => {
   }
   else {
     return(
-      <div className="w-full h-full">
-        <Line
-          datasetIdKey='id'
-          data={chartData}
-          options={options}
-          redraw={false}
-          height={"100px"}
-        />
-      </div>
+      <>
+        {/* CHART */}
+        <div className="w-full h-full">
+          <Line
+            datasetIdKey='id'
+            data={chartData}
+            options={options}
+            redraw={false}
+            height={"100px"}
+          />
+        </div>
+        {/* TABLE */}
+        <div className="w-full">
+          <div className="form-control w-full max-w-xs mb-8">
+            <label className="label">
+              <span className="label-text text-lg">Search for charger or status</span>
+            </label>
+            <input type="text" placeholder="Search" onInput={(e) => {setSearchFilter(e.target.value); updateFilter()}} className="input input-bordered w-full max-w-xs" />
+          </div>
+          <div className="overflow-x-auto w-full h-96">
+            <table className="table table-zebra w-full">
+              <thead>
+                {table.getHeaderGroups().map((headerGroup, header_group_id) => (
+                  <tr className="sticky top-0" key={header_group_id}>
+                  {headerGroup.headers.map((header, header_id) => (
+                      <th key={`header-${header_id}-header-group-${header_group_id}`}>
+                        {header.isPlaceholder ? null : <div className={ header.column.getCanSort() ? 'cursor-pointer select-none' : ''} >{flexRender(header.column.columnDef.header, header.getContext())}</div>}
+                        {{ asc: <i className="fa-solid fa-arrow-down"></i>, desc: <i className="fa-solid fa-arrow-up"></i> }[header.column.getIsSorted()] ?? null}
+                      </th>
+                  ))}
+                  </tr>
+                ))}
+              </thead>
+              <tbody>
+                {table.getRowModel().rows.map((row, rowIndex) => (
+                  <tr
+                    className="hover cursor-pointer select-none"
+                    key={rowIndex}
+                  >
+                    {row.getVisibleCells().map((cell, colIndex) => (
+                      <td key={`charger-table-${rowIndex}-${colIndex}`}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </>
+
     )
   }
 }
